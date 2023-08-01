e45a1382860b2c8451fab4fef77618423453ece4
YuliaDuk <yule444ka1234@mail.ru>
2023-08-02 00:43:06 +0300

    добавлен чекбокс в тег form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,13 +6,16 @@ function SearchForm () {
     return (
         <div className='search-form'>
             <form className='form-film'>
-                <input className='form-film__input' type='text' placeholder='Фильм' />
-                <button className='form-film__btn' type='button'>Найти</button>
+                <div className='form-film__container'>
+                    <input className='form-film__input' type='text' placeholder='Фильм' />
+                    <button className='form-film__btn' type='button'>Найти</button>
+                </div>
+                <div className='form-film__checkbox'>
+                    <FilterCheckbox />
+                    <p className='form-film__title'>Короткометражки</p>
+                </div>
             </form>
-            <div className='short-films'>
-                <FilterCheckbox />
-                <p className='short-films__title'>Короткометражки</p>
-            </div>
+            
         </div>
     )
 }
